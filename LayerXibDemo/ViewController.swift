237e8cca442f97eefd61ae3c4f5a29c3a238e3de
jqsir <812294683@qq.com>
2018-12-13 12:34:08 +0800

    Initial Commit

---
+++ LayerXibDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LayerXibDemo
+//
+//  Created by 江其 on 2018/12/13.
+//  Copyright © 2018 J. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
